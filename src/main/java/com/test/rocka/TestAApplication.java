b3ddfb93aea140207a0a07458b3e980da6e9d433
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2025-06-10 10:10:07 +0800

    修改提交

--- src/main/java/com/test/rocka/TestAApplication.java
+++ src/main/java/com/test/rocka/TestAApplication.java
@@ -1,6 +1,5 @@
 package com.test.rocka;
 
-import com.rock.rpc.annotation.EnableRpc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +7,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.test.rocka"})
 @Slf4j
-@EnableRpc
 public class TestAApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(TestAApplication.class, args);
