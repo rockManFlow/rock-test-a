6030e05c23bb975cb4cfa18836352213b2b6e27e
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2025-06-12 16:44:08 +0800

    测试多分支部署

--- src/main/java/com/test/rocka/TestAApplication.java
+++ src/main/java/com/test/rocka/TestAApplication.java
@@ -3,13 +3,12 @@ package com.test.rocka;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"com.test.rocka"})
 @Slf4j
 public class TestAApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(TestAApplication.class, args);
-        log.info("TestAApplication run proId:{}",context.getId());
+        SpringApplication.run(TestAApplication.class, args);
+        log.info("TestAApplication run");
     }
 }
